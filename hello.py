302ee496029fcfe3cea4548b9c2fa1a463cf4044
Lillie Schachter <7146649+lschachter@users.noreply.github.com>
2023-03-15 17:09:32 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,3 +3,10 @@ def hello_world():
     # WRITE ALL OF YOUR CODE (INDENTED) HERE
     # print out the string 'Hello World!'
     
+#hello_world()
+
+# After you've written your code, delete the `#` at the start of the line above this one to 'run' the code you've written.
+# Once you've saved this change, 'run' your code in your terminal by
+# 1. `cd`ing into the folder that this file is in
+# 2. Typing your computer's version of `python3 hello.py` into the terminal, and hitting enter
+# NOTE: look at the README to understand what 'your computer's version of' that command means.
